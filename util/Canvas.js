bf3cba4fe519bb36d3b221dd8b2aba1f905bc138
Tomusdrw <tomusdrw@gmail.com>
2011-02-09 21:53:22 +0100

    Added new drawing functions. Fixed state saving.

--- util/Canvas.js
+++ util/Canvas.js
@@ -1,22 +1,46 @@
 bs.util.Canvas = function(canv) {
 	this.canvas = bs.$(canv);
 	this.ctx = this.canvas[0].getContext('2d');
+	this.ctx.lineCap = "round";
+	this.ctx.lineJoin = "round";
 };
 
 bs.util.Canvas.prototype = bs.Object.create({
 	canvas: null,
 	ctx: null,
+	
+	setStrokeStyle: function(stroke) {
+		this.ctx.strokeStyle = stroke;
+	},
+	setFillStyle: function(fill) {
+		this.ctx.fillStyle = fill;
+	},
+	drawCircle: function(x, y, radius, fill) {
+		this.ctx.save();
+		this.ctx.beginPath();
+		this.ctx.arc(x, y, radius, 0, 2*Math.PI, true);
+		this.ctx.stroke();
+
+		this.ctx.restore();
+	},
 	fillCircle: function(x, y, radius, fill) {
-		var t = this.ctx.fillStyle;
-		//changing fillStyle
-		if (fill !== undefined) {
-			this.ctx.fillStyle = fill;
-		}
+		this.ctx.save();
 		this.ctx.beginPath();
 		this.ctx.arc(x, y, radius, 0, 2*Math.PI, true);
 		this.ctx.fill();
-		//restoring fillStyle
-		this.ctx.fillStyle = t;
-		return {type: 1, x: x, y: y, radius: radius};
+
+		this.ctx.restore();
+	},
+	drawLine: function(startX, startY, endX, endY, width, stroke) {
+		this.ctx.save();
+		if (!this.isUndefined(width)) {
+			this.ctx.lineWidth = width;
+		}
+		this.ctx.beginPath();
+		this.ctx.moveTo(startX, startY);
+		this.ctx.lineTo(endX, endY);
+		this.ctx.stroke();
+
+		this.ctx.restore();
 	}
 });
